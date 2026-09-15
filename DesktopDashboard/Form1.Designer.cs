ff3d3aaa5fce2e0d846c9805a1a2d7510fc05fdb
dotdyl <dotdyldev@gmail.com>
2026-09-15 16:47:45 -0700

    Opening and capturing new and existing applications into dashboard window
    List of currently open applications allows easy re-parenting to the dashboard window, and opening new applications only requires navigating to the correct file path of the executable.
    
    Current issues exist with certain popups or other windows that are apart of certain applications (discord, for example, has a startup window that gets captured rather than the actual application when embedding from startup).
    
    Aiming to add better configuration and grouping for embedded windows, as well as proper unparenting procedures when the dashboard is closed.

--- DesktopDashboard/Form1.Designer.cs
+++ DesktopDashboard/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.containerPanel = new System.Windows.Forms.Panel();
+            this.launchButton = new System.Windows.Forms.Button();
+            this.openApplicationsList = new System.Windows.Forms.ListBox();
+            this.openApplicationsHeader = new System.Windows.Forms.Label();
+            this.embedOpenAppsList = new System.Windows.Forms.ListBox();
+            this.embedOpenAppsHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // containerPanel
+            // 
+            this.containerPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.containerPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.containerPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.containerPanel.Location = new System.Drawing.Point(156, 12);
+            this.containerPanel.Name = "containerPanel";
+            this.containerPanel.Size = new System.Drawing.Size(993, 603);
+            this.containerPanel.TabIndex = 0;
+            this.containerPanel.SizeChanged += new System.EventHandler(this.ContainerPanel_SizeChanged);
+            // 
+            // launchButton
+            // 
+            this.launchButton.Location = new System.Drawing.Point(12, 12);
+            this.launchButton.Name = "launchButton";
+            this.launchButton.Size = new System.Drawing.Size(138, 39);
+            this.launchButton.TabIndex = 0;
+            this.launchButton.Text = "Open and Embed App";
+            this.launchButton.UseVisualStyleBackColor = true;
+            this.launchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // openApplicationsList
+            // 
+            this.openApplicationsList.FormattingEnabled = true;
+            this.openApplicationsList.Location = new System.Drawing.Point(12, 227);
+            this.openApplicationsList.Name = "openApplicationsList";
+            this.openApplicationsList.Size = new System.Drawing.Size(138, 95);
+            this.openApplicationsList.TabIndex = 0;
+            // 
+            // openApplicationsHeader
+            // 
+            this.openApplicationsHeader.AutoSize = true;
+            this.openApplicationsHeader.Location = new System.Drawing.Point(12, 211);
+            this.openApplicationsHeader.Name = "openApplicationsHeader";
+            this.openApplicationsHeader.Size = new System.Drawing.Size(93, 13);
+            this.openApplicationsHeader.TabIndex = 0;
+            this.openApplicationsHeader.Text = "Open Applications";
+            // 
+            // embedOpenAppsList
+            // 
+            this.embedOpenAppsList.FormattingEnabled = true;
+            this.embedOpenAppsList.Location = new System.Drawing.Point(12, 86);
+            this.embedOpenAppsList.Name = "embedOpenAppsList";
+            this.embedOpenAppsList.Size = new System.Drawing.Size(138, 95);
+            this.embedOpenAppsList.TabIndex = 0;
+            this.embedOpenAppsList.SelectedIndexChanged += new System.EventHandler(this.embedOpenAppsList_SelectedIndexChanged);
+            // 
+            // embedOpenAppsHeader
+            // 
+            this.embedOpenAppsHeader.AutoSize = true;
+            this.embedOpenAppsHeader.Location = new System.Drawing.Point(12, 70);
+            this.embedOpenAppsHeader.Name = "embedOpenAppsHeader";
+            this.embedOpenAppsHeader.Size = new System.Drawing.Size(96, 13);
+            this.embedOpenAppsHeader.TabIndex = 1;
+            this.embedOpenAppsHeader.Text = "Embed Open Apps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1170, 627);
+            this.Controls.Add(this.embedOpenAppsHeader);
+            this.Controls.Add(this.embedOpenAppsList);
+            this.Controls.Add(this.openApplicationsHeader);
+            this.Controls.Add(this.openApplicationsList);
+            this.Controls.Add(this.launchButton);
+            this.Controls.Add(this.containerPanel);
+            this.Name = "Form1";
+            this.Text = "Desktop Dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel containerPanel;
+        private System.Windows.Forms.Button launchButton;
+        private System.Windows.Forms.ListBox openApplicationsList;
+        private System.Windows.Forms.Label openApplicationsHeader;
+        private System.Windows.Forms.ListBox embedOpenAppsList;
+        private System.Windows.Forms.Label embedOpenAppsHeader;
     }
 }
 
--- DesktopDashboard/Form1.cs
+++ DesktopDashboard/Form1.cs
@@ -2,36 +2,177 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DesktopDashboard
 {
-    public struct Zone
-    {
-        public int X;
-        public int Y;
-        public int Width;
-        public int Height;
-
-        public Zone(int x, int y, int width, int height) {
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
-        }
-    }
-
-
 
     public partial class Form1 : Form
     {
+        // Importing Win32 API functions
+        #region Win32 API Functions
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        #endregion
+
+        // Win32 Constants
+        private const int GWL_STYLE = -16;
+        private const int WS_POPUP = 0x8000000;
+        private const int WS_CHILD = 0x40000000;
+
+        private IntPtr embeddedAppHandle = IntPtr.Zero;
+
         public Form1()
         {
+            // Initialize the main form and it's componenets
             InitializeComponent();
+
+            EnumWindows((hWnd, lParam) =>
+            {
+                // Only inspect windows that the user can actually see
+                if (IsWindowVisible(hWnd))
+                {
+                    GetWindowThreadProcessId(hWnd, out uint processId);
+
+                    try
+                    {
+                        Process proc = Process.GetProcessById((int)processId);
+                        string processName = proc.ProcessName;
+
+                        // Optional: Print open apps to terminal to see what's discoverable
+                        if (!string.IsNullOrEmpty(proc.MainWindowTitle))
+                        {
+                            Console.WriteLine($"Found App: {processName} | Title: {proc.MainWindowTitle}");
+                            embedOpenAppsList.Items.Add(proc.MainWindowTitle);
+                        }
+                    }
+                    catch
+                    {
+                        // Ignore system processes that deny access permissions
+                    }
+                }
+                return true; // Keep looping through other windows
+            }, IntPtr.Zero);
+        }
+
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                // Start the target process
+                Process proc = Process.Start(od.FileName);
+
+                // Give the application a brief moment to create its main UI thread window handle
+                proc.WaitForInputIdle();
+                while (proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    Thread.Sleep(50);
+                    proc.Refresh();
+                }
+
+                embeddedAppHandle = proc.MainWindowHandle;
+
+                // Remove native borders and title bars so it acts as an integrated child element
+                int style = GetWindowLong(embeddedAppHandle, GWL_STYLE);
+                style = (style & ~WS_POPUP) | WS_CHILD;
+                //SetWindowLong(embeddedAppHandle, GWL_STYLE, style);
+
+                // Bind the external program window to live inside your local layout UI panel
+                SetParent(embeddedAppHandle, containerPanel.Handle);
+
+                // Force the newly trapped window to fill out the workspace boundaries completely
+                ResizeEmbeddedApp();
+
+                Console.WriteLine($"Successfully started and captured '{od.FileName}'");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to start or capture '{od.FileName}'");
+            }
+        }
+
+        private void ContainerPanel_SizeChanged(object sender, EventArgs e)
+        {
+            ResizeEmbeddedApp();
+        }
+
+        private void ResizeEmbeddedApp()
+        {
+            if (embeddedAppHandle != IntPtr.Zero)
+            {
+                // Align the position perfectly over the parent boundaries
+                MoveWindow(embeddedAppHandle, 0, 0, containerPanel.Width, containerPanel.Height, true);
+            }
+        }
+
+        private void embedOpenAppsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (embedOpenAppsList.SelectedIndex != -1)
+            {
+                string selectedValue = embedOpenAppsList.SelectedItem.ToString();
+                int selectedIdx = embedOpenAppsList.SelectedIndex;
+                Console.WriteLine($"Selected index {selectedIdx} : {selectedValue}");
+
+                IntPtr foundWindowHandle = IntPtr.Zero;
+
+                EnumWindows((hWnd, lParam) =>
+                {
+                    // Only inspect windows that the user can actually see
+                    if (IsWindowVisible(hWnd))
+                    {
+                        GetWindowThreadProcessId(hWnd, out uint processId);
+
+                        try
+                        {
+                            Process proc = Process.GetProcessById((int)processId);
+                            string processName = proc.MainWindowTitle;
+
+                            // Match against our target application
+                            if (processName.Equals(selectedValue, StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Found target application!");
+                                foundWindowHandle = hWnd;
+                                SetParent(hWnd, containerPanel.Handle);
+                                return false; // Stop looping early, we found our window!
+                            }
+                        }
+                        catch
+                        {
+                            // Ignore system processes that deny access permissions
+                        }
+                    }
+                    return true; // Keep looping through other windows
+                }, IntPtr.Zero);
+            }
         }
     }
 }
