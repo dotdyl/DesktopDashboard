a9727f520318744233d99d49f19e6f2bacf1ec26
dotdyl <dotdyldev@gmail.com>
2026-09-15 14:56:42 -0700

    Add project files.

---
+++ DesktopDashboard/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopDashboard
+{
+    public struct Zone
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+        public Zone(int x, int y, int width, int height) {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
